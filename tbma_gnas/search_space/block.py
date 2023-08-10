2d75efe5bd565abc41cb70b19e856dc50e03253b
Adrián José Martínez Navarro <admartnav@gmail.com>
2023-08-10 20:45:07 +0200

    Query for single layer almost ready. Missing some tests and removal of print statements.

--- tbma_gnas/search_space/block.py
+++ tbma_gnas/search_space/block.py
@@ -15,6 +15,11 @@ class LearnableBlock:
 
         self.activation = LearnableSpaceComponent(ComponentType.ACTIVATION)
 
+        self.prev_layer = None
+        self.prev_hyperparameters = None
+        self.prev_out_channels = None
+        self.prev_act = None
+
     def get_input(self):
         return self.is_input
 
@@ -35,12 +40,21 @@ class LearnableBlock:
 
         self.activation.learn(component=activation.__class__.__name__, positive=positive)
 
+    def rebuild_block(self, new_in_channels: int):
+        if self.prev_layer and self.prev_act:
+            return self.prev_layer(in_channels=new_in_channels, out_channels=self.prev_out_channels,
+                                   **self.prev_hyperparameters), self.prev_act()
+
     def query(self, prev_out_channels: int, output_shape: int):
         init_layer = self.layer.query()
         dim_ratio, params = self.layer_hyperparameters.query_for_layer(init_layer.__name__)
         out_channels = output_shape if self.is_output else (
             prev_out_channels if dim_ratio == DimensionRatio.EQUAL else max(prev_out_channels // 2, output_shape))
-
         init_act = self.activation.query()
+
+        self.prev_layer = init_layer
+        self.prev_hyperparameters = params
+        self.prev_out_channels = out_channels
+        self.prev_act = init_act
 
         return init_layer(in_channels=prev_out_channels, out_channels=out_channels, **params), init_act()
--- tbma_gnas/search_space/search_space.py
+++ tbma_gnas/search_space/search_space.py
@@ -1,4 +1,5 @@
 import copy
+import random
 from threading import Lock
 
 from .block import LearnableBlock
@@ -16,7 +17,7 @@ class SearchSpace:
         self.space = {1: [LearnableBlock(is_input=True, is_output=True)]}
         self.lock = Lock()
 
-    def learn_for_depth(self, model: HyperModel, positive: bool):
+    def learn(self, model: HyperModel, positive: bool):
         with self.lock:
             depth = len(model.get_blocks())
             for lay_act, block in zip(model.get_blocks(), self.space[depth]):
@@ -39,4 +40,48 @@ class SearchSpace:
                 gen_block = block.query(prev_out_channels=heads * prev_channels, output_shape=self.output_shape)
                 model.append(gen_block)
 
-            return HyperModel(model)
+            return HyperModel(model_blocks=model)
+
+    def query_single_layer(self, model: HyperModel) -> HyperModel:
+        blocks = model.get_blocks()
+
+        print(blocks)
+
+        model_depth = len(blocks)
+        block_idx = random.randint(0, model_depth - 1)
+        print("Index:", block_idx)
+
+        old_block_heads = blocks[block_idx][0].heads if has_heads_parameter(blocks[block_idx][0]) else 1
+        old_block_out_shape = old_block_heads * blocks[block_idx][0].out_channels
+        print("Old output shape:", old_block_out_shape)
+
+        prev_block_heads = 1
+        if block_idx > 0:
+            prev_block_heads = blocks[block_idx - 1][0].heads if has_heads_parameter(
+                blocks[block_idx - 1][0]) else prev_block_heads
+        print("Previous block heads:", prev_block_heads)
+
+        new_in_channels = self.num_node_features if block_idx == 0 else prev_block_heads * blocks[block_idx - 1][
+            0].out_channels
+        new_block = self.space[model_depth][block_idx].query(prev_out_channels=new_in_channels,
+                                                             output_shape=self.output_shape)
+        blocks[block_idx] = new_block
+
+        new_block_heads = new_block[0].heads if has_heads_parameter(new_block[0]) else 1
+        new_block_out_shape = new_block_heads * new_block[0].out_channels
+        print("New output shape:", new_block_out_shape)
+        if new_block_out_shape != old_block_out_shape and block_idx != model_depth - 1:
+            print("Adjusting next block")
+            blocks[block_idx + 1] = self.space[model_depth][block_idx + 1].rebuild_block(
+                new_in_channels=new_block_heads * blocks[block_idx][0].out_channels)
+
+        print(blocks)
+
+        return HyperModel(model_blocks=blocks)
+
+
+space = SearchSpace(num_node_features=25, output_shape=5)
+_ = space.query_model_for_depth(depth=2)
+_ = space.query_model_for_depth(depth=3)
+test_model = space.query_model_for_depth(depth=4)
+layer_changed = space.query_single_layer(test_model)
